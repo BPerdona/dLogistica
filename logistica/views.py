3b3784d5e4d150819c8ecc66b6b10b5e19b444dc
Bruno Perdona <bruno15perdona@gmail.com>
2022-02-27 10:45:46 -0300

    Add Pagination on Paciente and Consulta Views

--- logistica/views.py
+++ logistica/views.py
@@ -1,6 +1,7 @@
 from urllib import request
 from django.shortcuts import render
 from logistica.models import Paciente, Consulta, Viagem, Motorista
+from django.core.paginator import Paginator
 
 def index(request):
     qtd_pacientes = Paciente.objects.count()
@@ -18,20 +19,37 @@ def index(request):
     return render(request, 'logistica/index.html', context)
 
 def listPaciente(request):
+    #Filtragem
     pacientes = Paciente.objects.order_by('-id').filter(
         status_tupla=True
     )
+
+    #Paginação
+    paginator = Paginator(pacientes, 10)
+    page = request.GET.get('page')
+    pacientes = paginator.get_page(page)
+
+    #renderização da pagina
     return render(request, 'logistica/paciente.html', {
         'pacientes' : pacientes
     })
 
 def buscaPaciente(request):
+    #renderização da pagina
     return render(request, 'logistica/buscaPaciente.html')
 
 def listConsulta(request):
+    #Filtragem
     consultas = Consulta.objects.order_by('-id').filter(
         status_tupla=True
     )
+
+    #Paginação
+    paginator = Paginator(consultas, 10)
+    page = request.GET.get('page')
+    consultas = paginator.get_page(page)
+
+    #renderização da pagina
     return render(request, 'logistica/consulta.html', {
         'consultas' : consultas
     })
