09e1a37bf8ebd39a1fa24b91a78e28a2168ba0c6
Bruno Perdona <bruno15perdona@gmail.com>
2022-03-02 14:25:30 -0300

    Fixing forms date and adding django.admin

--- logistica/admin.py
+++ logistica/admin.py
@@ -1,3 +1,16 @@
+from re import search
 from django.contrib import admin
+from .models import Consulta, Paciente
 
-# Register your models here.
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_completo', 'rg', 'cpf', 'cns', 'status_tupla')
+    list_display_links = ('id', 'nome_completo', 'rg', 'cns', 'cpf')
+    search_fields = ('id', 'nome_completo', 'rg', 'cpf', 'cns')
+
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data_da_consulta', 'paciente', 'hospital', 'horario_da_consulta', 'status_tupla')
+    list_display_links = ('id', 'data_da_consulta', 'paciente', 'hospital', 'horario_da_consulta')
+    search_fields = ('id', 'data_da_consulta', 'hospital')
+
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Consulta, ConsultaAdmin)
